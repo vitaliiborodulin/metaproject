04ca2dc535bfca2dbb5a1fbc328bc2a6aa7b413b
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-07-31 14:17:53 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,32 @@ $(function() {
 	if ($(window).width() > 767){
 		new WOW().init();
 	}
+	var swiper = new Swiper(".swiper", {
+		// loop: "true",
+		// effect: "coverflow",
+		// grabCursor: true,
+		// centeredSlides: true,
+		// slidesPerView: "auto",
+		// coverflowEffect: {
+		// 	rotate: 50,
+		// 	stretch: 0,
+		// 	depth: 100,
+		// 	modifier: 1,
+		// 	slideShadows: true,
+		// },
+		// pagination: {
+		// 	el: ".swiper-pagination",
+		// },
+		effect: "cards",
+	  grabCursor: true,
+		navigation: {
+			nextEl: ".case-button-next",
+			prevEl: ".case-button-prev",
+		},
+		pagination: {
+			el: ".case-pagination",
+			clickable: true,
+		},
+	});
 
 });
