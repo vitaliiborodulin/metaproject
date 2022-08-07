838f954ff504858c9a06a39e5425585092078db3
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-08-07 13:19:47 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,21 @@ $(function() {
 	});
 	
 	wow.init();
+	const burger = $('.header__burger');
+	const menu = $('.header__menu');
 	
+	burger.on('click', function(e) {
+	    e.preventDefault();
+	    burger.toggleClass('header__burger--close');
+	    menu.toggleClass('open');
+	});
+	
+	$(document).keyup(function(e) {
+	    if (e.key === "Escape" || e.keyCode === 27) {
+	        menu.removeClass('open');
+	        burger.removeClass('header__burger--close');
+	    }
+	});
 	
 	$('body').on('click', '.header__menu a[href*="#"]', function(e) {
 		var fixed_offset = 70;
@@ -18,8 +32,8 @@ $(function() {
 				scrollTop: $(this.hash).offset().top - fixed_offset
 		}, 300);
 		e.preventDefault();
-		// burger.toggleClass('header__burger--close');
-		// menu.toggleClass('nav--open');
+		menu.removeClass('open');
+		burger.removeClass('header__burger--close');
 	});
 	
 	$('body').on('click', '.footer__menu a[href*="#"]', function(e) {
@@ -29,8 +43,6 @@ $(function() {
 				scrollTop: $(this.hash).offset().top - fixed_offset
 		}, 300);
 		e.preventDefault();
-		// burger.toggleClass('header__burger--close');
-		// menu.toggleClass('nav--open');
 	});
 	var swiper = new Swiper(".swiper", {
 		// loop: "true",
