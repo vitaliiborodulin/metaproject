94a8121c2218745b9ff8ddf2f9f4c5dbec9ab5c6
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-08-07 11:50:57 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
 $(function() {
 
-	if ($(window).width() > 767){
-		new WOW().init();
-	}
+	wow = new WOW({
+		boxClass:     'wow',      // default
+		animateClass: 'animated', // default
+		offset:       200,          // default
+		mobile:       false,       
+		live:         true        // default
+	});
+	
+	wow.init();
+	
+	
+	$('body').on('click', '.header__menu a[href*="#"]', function(e) {
+		var fixed_offset = 70;
+	
+		$('html,body').stop().animate({
+				scrollTop: $(this.hash).offset().top - fixed_offset
+		}, 300);
+		e.preventDefault();
+		// burger.toggleClass('header__burger--close');
+		// menu.toggleClass('nav--open');
+	});
+	
+	$('body').on('click', '.footer__menu a[href*="#"]', function(e) {
+		var fixed_offset = 70;
+	
+		$('html,body').stop().animate({
+				scrollTop: $(this.hash).offset().top - fixed_offset
+		}, 300);
+		e.preventDefault();
+		// burger.toggleClass('header__burger--close');
+		// menu.toggleClass('nav--open');
+	});
 	var swiper = new Swiper(".swiper", {
 		// loop: "true",
 		// effect: "coverflow",
